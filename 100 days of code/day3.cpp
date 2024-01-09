f58c23fa03c90b3ddf0475d4ef3ac17d58d73f09
AMARJEET KUMAR <108997919+Amarjeet113@users.noreply.github.com>
2024-01-09 21:37:03 +0530

    Add files via upload

---
+++ 100 days of code/day3.cpp
@@ -0,0 +1,19 @@
+// power of two
+
+class Solution
+{
+public:
+    bool isPowerOfTwo(int n)
+    {
+        for (int i = 0; i <= 30; i++)
+        {
+            int ans = pow(2, i);
+
+            if (ans == n)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+};
