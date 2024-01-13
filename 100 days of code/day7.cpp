b41d9fde12fc1581bb2d8600aa41f0322d0805ed
AMARJEET KUMAR <108997919+Amarjeet113@users.noreply.github.com>
2024-01-13 20:03:57 +0530

    Update day7.cpp

---
+++ 100 days of code/day7.cpp
@@ -0,0 +1,19 @@
+// delete node in a linked list
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        node->val=node->next->val;
+        node->next=node->next->next;
+        
+        
+    }
+};
