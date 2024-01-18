b5f16fb4e557910b42f41831e5696cfa74d083b5
AMARJEET KUMAR <108997919+Amarjeet113@users.noreply.github.com>
2024-01-18 20:07:52 +0530

    Update day12.cpp

--- 100 days of code/day12.cpp
+++ 100 days of code/day12.cpp
@@ -1,1 +1,17 @@
 //  Longest Substring Without Repeating Characters
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int x = 0;
+        vector<int>count(256);
+        for (int i=0,r=0;r<s.length();++r){
+            ++count[s[r]];
+            while (count[s[r]]>1)
+            --count[s[i++]];
+            x= max(x,r-i+1);
+        }
+        return x;
+        
+    }
+};
