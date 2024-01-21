41906bf72b40f44b638edeeb20637afd3827c947
AMARJEET KUMAR <108997919+Amarjeet113@users.noreply.github.com>
2024-01-21 20:11:52 +0530

    Update day15.cpp

--- 100 days of code/day15.cpp
+++ 100 days of code/day15.cpp
@@ -1,1 +1,18 @@
-// 
+//  ADD DIGITS
+
+class Solution {
+public:
+    int addDigits(int num) {
+        if (num==0){
+            return 0;
+        }
+        else if (num%9==0){
+            return 9;
+        }
+        else{
+            int ans=num%9;
+            return ans;
+        }
+        
+    }
+};
