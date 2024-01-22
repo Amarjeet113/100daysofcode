9e6f1ab214a30e1e9f70f2116db729a39214821e
AMARJEET KUMAR <108997919+Amarjeet113@users.noreply.github.com>
2024-01-22 19:32:37 +0530

    Update day16.cpp

--- 100 days of code/day16.cpp
+++ 100 days of code/day16.cpp
@@ -1,1 +1,23 @@
 // LONGEST COMMON PREFIX
+
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+      string x= "";
+      int n = strs.size();
+      sort(strs.begin(),strs.end());
+      string first= strs[0];
+      string last=strs[n-1];
+      for(int i =0;i<first.size();i++){
+          if(first[i]!=last[i]){
+              return x ;
+          }
+          x+=first[i];
+      }
+      return x ;
+
+
+      
+     
+    }
+};
