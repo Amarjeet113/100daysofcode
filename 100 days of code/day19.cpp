0b510429011736da5016b2bf7ba825f5a467f8cc
AMARJEET KUMAR <108997919+Amarjeet113@users.noreply.github.com>
2024-01-25 09:25:48 +0530

    Update day19.cpp

--- 100 days of code/day19.cpp
+++ 100 days of code/day19.cpp
@@ -1,1 +1,20 @@
 // Complement of Base 10 Integer
+
+class Solution {
+public:
+    int bitwiseComplement(int n) {
+        int x = n;
+        int var = 0;
+        //base case 
+        if (n==0)
+        return 1;
+        while (x!=0){
+            var = (var<<1)| 1;
+            x= x>>1;
+        }
+        int ans = (~n) & var;
+        return ans;
+
+        
+    }
+};
