3cd923f4dc08faf347283cc7bc448eb118ca7cda
AMARJEET KUMAR <108997919+Amarjeet113@users.noreply.github.com>
2024-01-30 22:55:39 +0530

    Update day21.cpp

--- 100 days of code/day21.cpp
+++ 100 days of code/day21.cpp
@@ -1,1 +1,19 @@
-//
+//  FIBONACCI NUMBER
+
+Class Solution {
+public:
+    int fib(int n) {
+        //base case 
+        if (n==0){
+            return 0;
+        }
+        if (n==1){
+            return 1;
+        }
+        int x = fib(n-1)+fib(n-2);
+        return x;
+           
+
+        
+    }
+};
