7c1613cb490375b896d25c02ce8a1a7cd5535a50
AMARJEET KUMAR <108997919+Amarjeet113@users.noreply.github.com>
2024-01-30 22:54:42 +0530

    Update day20.cpp

--- 100 days of code/day20.cpp
+++ 100 days of code/day20.cpp
@@ -1,1 +1,15 @@
 //  MOVE ZEROES
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int i = 0;
+        for (int j=0; j<nums.size();j++){
+            if (nums[j]!=0){
+                swap(nums[j],nums[i]);
+                i++;
+            }
+        }
+
+    }
+};
