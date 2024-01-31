a2ca03182683c83105a197540e21f4c1de1c6e02
AMARJEET KUMAR <108997919+Amarjeet113@users.noreply.github.com>
2024-01-31 23:35:39 +0530

    Update day23.cpp

--- 100 days of code/day23.cpp
+++ 100 days of code/day23.cpp
@@ -1,1 +1,15 @@
 //  reverse string
+
+class Solution {
+public:
+    void reversestring(vector<char> &str, int i, int j){
+        if(i > j) return;
+        swap(str[i], str[j]);
+        i++;
+        j--;
+        reversestring(str, i, j);
+    }
+    void reverseString(vector<char>& s) {
+        reversestring(s, 0, s.size()-1);
+    }
+};
