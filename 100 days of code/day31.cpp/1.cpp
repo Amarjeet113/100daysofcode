e4e7fc7ebfdd3037a0b10e9e97373a9a59a541c8
AMARJEET KUMAR <108997919+Amarjeet113@users.noreply.github.com>
2024-02-07 23:22:39 +0530

    Update 1.cpp

--- 100 days of code/day31.cpp/1.cpp
+++ 100 days of code/day31.cpp/1.cpp
@@ -1,1 +1,24 @@
 // short characters by frequency
+
+class Solution {
+public:
+  std::string frequencySort(std::string s) {
+        std::unordered_map<char, int> frequency;
+        
+        // Count the frequency of each character in the string
+        for(const char& c: s) {
+            frequency[c]++;
+        }
+        
+        // Sort the characters in decreasing order based on frequency
+        std::sort(s.begin(), s.end(), [&](const char& a, const char& b) {
+            if (frequency[a] != frequency[b]) {
+                return frequency[a] > frequency[b];
+            }
+            return a < b;
+        });
+        
+        return s;
+    }
+
+};
