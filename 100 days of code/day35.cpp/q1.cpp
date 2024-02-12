5508e657c51a2063c8bc9abef35607bfcdf44e4d
AMARJEET KUMAR <108997919+Amarjeet113@users.noreply.github.com>
2024-02-12 23:22:26 +0530

    Update q1.cpp

--- 100 days of code/day35.cpp/q1.cpp
+++ 100 days of code/day35.cpp/q1.cpp
@@ -1,2 +1,10 @@
 //  Majority Element
 
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        return nums[(int)nums.size()/2];
+        
+    }
+};
