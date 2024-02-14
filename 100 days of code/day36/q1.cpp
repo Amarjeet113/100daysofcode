354dd493044bc5579f5a1ec5303e0c616079d389
AMARJEET KUMAR <108997919+Amarjeet113@users.noreply.github.com>
2024-02-14 21:54:15 +0530

    Update q1.cpp

--- 100 days of code/day36/q1.cpp
+++ 100 days of code/day36/q1.cpp
@@ -1,1 +1,26 @@
 //
+
+class Solution {
+public:
+    // Function to clone a graph
+    Node* cloneGraph(Node* node) {
+        // If the node is NULL, return NULL
+        if (!node) {
+            return NULL;
+        }
+        // If the node is not present in the map, create a copy of it
+        if (copies.find(node) == copies.end()) {
+            // Create a new node with the same value as the original node
+            copies[node] = new Node(node -> val, {});
+            // Copy the neighbors of the original node recursively
+            for (Node* neighbor : node -> neighbors) {
+                copies[node] -> neighbors.push_back(cloneGraph(neighbor));
+            }
+        }
+        // Return the copy of the node
+        return copies[node];
+    }
+private:
+    // Map to store the copy of the nodes
+    unordered_map<Node*, Node*> copies;
+};
