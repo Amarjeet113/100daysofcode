1078df6c2f04df5ad550bbfa56c75d617310ed97
AMARJEET KUMAR <108997919+Amarjeet113@users.noreply.github.com>
2024-02-14 21:56:25 +0530

    Update q2.cpp

--- 100 days of code/day36/q2.cpp
+++ 100 days of code/day36/q2.cpp
@@ -1,1 +1,18 @@
-//
+//  
+
+
+class Solution {
+public:
+    string firstPalindrome(vector<string>& words) {
+        for(auto i: words){
+            string rev =i;
+            reverse(rev.begin(),rev.end());
+
+            if(rev == i){
+                return i;
+            }
+        }
+         return "";
+            
+    }
+};
