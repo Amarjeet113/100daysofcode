e5feae5294ad84138c6fa154eeed2c03d11cec2f
AMARJEET KUMAR <108997919+Amarjeet113@users.noreply.github.com>
2024-02-14 21:52:26 +0530

    Update q2.cpp

--- 100 days of code/day37/q2.cpp
+++ 100 days of code/day37/q2.cpp
@@ -1,1 +1,26 @@
 //
+
+
+class Solution {
+public:
+    vector<int> rearrangeArray(vector<int>& nums) {
+        int n = nums.size();
+        int pi = 0; //positive index
+        int ni = 1; //negative index
+        
+        
+        vector<int> result(n);
+        
+        for(int &num : nums) {
+            if(num > 0) {
+                result[pi] = num;
+                pi +=2;
+            } else {
+                result[ni] = num;
+                ni += 2;
+            }
+        }
+        
+        return result;   
+    }
+};
