cc5a3d0b57b621c817ffb3acf2e857ae53c96b86
AMARJEET KUMAR <108997919+Amarjeet113@users.noreply.github.com>
2024-02-15 21:12:26 +0530

    Update q1.cpp

--- 100 days of code/day38/q1.cpp
+++ 100 days of code/day38/q1.cpp
@@ -1,1 +1,30 @@
-// 
+// Find Polygon With the Largest Perimeter(LEETCODE)
+
+class Solution {
+public:
+    long long SumNums(vector<int>& nums){
+        long long total=0;
+
+         for(auto i : nums) total+=i;
+
+        return total;
+
+    }
+
+    long long largestPerimeter(vector<int>& nums) {
+
+        int n = nums.size();
+
+        sort(nums.begin(),nums.end());
+
+        long long total = SumNums(nums);
+
+        for(int i=n-1; i>= 2 ; i --){
+
+            if(total- nums[i] >nums[i]) return total;
+
+            total-=nums[i];
+        }
+        return -1;
+    }
+};
