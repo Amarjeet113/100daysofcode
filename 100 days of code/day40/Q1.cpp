418c4e7370f40406e20fbf752e53a492b58260e7
AMARJEET KUMAR <108997919+Amarjeet113@users.noreply.github.com>
2024-02-17 21:57:24 +0530

    Update Q1.cpp

--- 100 days of code/day40/Q1.cpp
+++ 100 days of code/day40/Q1.cpp
@@ -1,1 +1,31 @@
 // 1642. Furthest Building You Can Reach  (LEETCODE)
+
+class Solution {
+public:
+    int furthestBuilding(vector<int>& heights, int bricks, int ladders) {
+        priority_queue<int> ladder_gaps;
+        
+        for (int i = 0; i < heights.size() - 1; i++) {
+            int gap = heights[i + 1] - heights[i];
+            
+            if (gap > 0) {
+                if (ladders > 0) {
+                    ladder_gaps.push(-gap);
+                    ladders--;
+                } else if (!ladder_gaps.empty() && -ladder_gaps.top() < gap) {
+                    bricks += ladder_gaps.top();
+                    ladder_gaps.pop();
+                    ladder_gaps.push(-gap);
+                } else {
+                    bricks -= gap;
+                }
+                
+                if (bricks < 0) {
+                    return i;
+                }
+            }
+        }
+        
+        return heights.size() - 1;
+    }
+};
