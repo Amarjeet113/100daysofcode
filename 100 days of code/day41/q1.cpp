1dd72fd7010d5aebd1bc267c8b29129f86b4a4f6
AMARJEET KUMAR <108997919+Amarjeet113@users.noreply.github.com>
2024-02-18 22:53:59 +0530

    Update q1.cpp

---
+++ 100 days of code/day41/q1.cpp
@@ -0,0 +1,50 @@
+//  Meeting Rooms III (LEETCODE)
+
+class Solution {
+public:
+    int mostBooked(int n, vector<vector<int>>& meetings) {
+        sort(meetings.begin(), meetings.end());
+        vector<int> ans(n, 0);
+        
+        priority_queue<pair<long long, long long>, vector<pair<long long, long long>>, greater<pair<long long, long long>>> pq;
+        priority_queue<long long, vector<long long>, greater<long long>> pq2;
+        for(int i = 0; i < n; i++)
+            pq.push({0, i});
+
+        long long time = 0;
+        for(int i = 0; i < meetings.size(); i++){
+            time = max(time, (long long)meetings[i][0]);
+
+            while(pq.size() and time >= pq.top().first){
+                pq2.push(pq.top().second);
+                pq.pop();
+            }
+
+            if(pq2.empty()){
+                time = pq.top().first;
+                while(pq.size() and time >= pq.top().first){
+                    pq2.push(pq.top().second);
+                    pq.pop();
+                }                
+            }
+
+            int meet = pq2.top();
+            pq2.pop();
+
+            ++ans[meet];
+            pq.push({time + meetings[i][1] - meetings[i][0], meet});
+        }
+
+        int room = 0;
+        int MAX = 0;
+
+        for(int i = 0; i < n; i++){
+            if(ans[i] > MAX){
+                MAX = ans[i];
+                room = i;
+            }
+        }
+
+        return room;
+    }
+};
