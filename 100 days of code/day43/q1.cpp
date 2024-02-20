6a7b3b82b9fcf5776e26ef982f6dc169a3f7fddf
AMARJEET KUMAR <108997919+Amarjeet113@users.noreply.github.com>
2024-02-20 19:24:13 +0530

    Update q1.cpp

--- 100 days of code/day43/q1.cpp
+++ 100 days of code/day43/q1.cpp
@@ -1,1 +1,15 @@
-//  
+//  MISSING NUMBER (LEETCODE)
+
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+          int ans=0;
+        for(int i=0;i<nums.size();i++){
+            ans=ans^nums[i];
+        }
+        for(int i=1;i<=nums.size();i++){
+            ans=ans^i;
+        }
+        return ans;
+    }
+};
