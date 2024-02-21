fd3a72dc1fdd67a7e38975ff72fbced4affaafe7
AMARJEET KUMAR <108997919+Amarjeet113@users.noreply.github.com>
2024-02-21 18:49:09 +0530

    Update q1.cpp

--- 100 days of code/day44/q1.cpp
+++ 100 days of code/day44/q1.cpp
@@ -1,1 +1,14 @@
 //  201. Bitwise AND of Numbers Range(LEETCODE)
+
+class Solution {
+public:
+    int rangeBitwiseAnd(int left, int right) {
+        int shift = 0;
+        while (left < right) {
+            left >>= 1;
+            right >>= 1;
+            shift++;
+        }
+        return left << shift;
+    }
+};
