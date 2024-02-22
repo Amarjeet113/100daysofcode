a66f857233f0de134a5e3d75fa121483947b5920
AMARJEET KUMAR <108997919+Amarjeet113@users.noreply.github.com>
2024-02-22 19:17:34 +0530

    Update q1.cpp

--- 100 days of code/day45/q1.cpp
+++ 100 days of code/day45/q1.cpp
@@ -1,1 +1,21 @@
-// 
+//  Find the Town Judge  (LEETCODE)
+
+class Solution {
+public:
+    int findJudge(int n, std::vector<std::vector<int>>& trust) {
+        std::vector<int> trustCount(n + 1, 0);
+        
+        for(auto t : trust) {
+            trustCount[t[0]]--; // Decrease trust count for the person who trusts
+            trustCount[t[1]]++; // Increase trust count for the person who is trusted
+        }
+        
+        for(int i = 1; i <= n; i++) {
+            if(trustCount[i] == n - 1) {
+                return i;
+            }
+        }
+        
+        return -1;
+    }
+};
