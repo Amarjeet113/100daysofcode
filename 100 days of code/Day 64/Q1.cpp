7ff2f789270155ce75767018d6baf01058fe1204
AMARJEET KUMAR <108997919+Amarjeet113@users.noreply.github.com>
2024-03-21 23:26:14 +0530

    Update Q1.cpp

--- 100 days of code/Day 64/Q1.cpp
+++ 100 days of code/Day 64/Q1.cpp
@@ -1,1 +1,17 @@
 //  Binary Subarrays With Sum  (LEETCODE)
+
+class Solution {
+public:
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
+        unordered_map<int, int> count;
+        int sum = 0, res = 0;
+        
+        for (int num : nums) {
+            count[sum]++;
+            sum += num;
+            res += count[sum - goal];
+        }
+        
+        return res;
+    }
+};
