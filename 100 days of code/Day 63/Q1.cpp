7a191356317d5eb44ab2f3a4bfc8bc42200066b4
AMARJEET KUMAR <108997919+Amarjeet113@users.noreply.github.com>
2024-03-21 23:25:10 +0530

    Update Q1.cpp

---
+++ 100 days of code/Day 63/Q1.cpp
@@ -0,0 +1,14 @@
+//  Find the Pivot Integer  (LEETCODE)
+
+class Solution {
+public:
+    int pivotInteger(int n) {
+        double x = sqrt(n * (n + 1) / 2.0);
+        
+        if (fmod(x, 1.0) != 0) {
+            return -1;
+        } else {
+            return static_cast<int>(x);
+        }
+    }
+};
