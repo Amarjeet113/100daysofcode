0a30376576cf984dc0c8d90a24f5578537c026c8
AMARJEET KUMAR <108997919+Amarjeet113@users.noreply.github.com>
2024-03-30 21:50:53 +0530

    Update q2.cpp

--- 100 days of code/Day67/q2.cpp
+++ 100 days of code/Day67/q2.cpp
@@ -1,3 +1,23 @@
 //  Delete without head pointer  (GFG)
 
 
+class Solution
+{
+    public:
+    //Function to delete a node without any reference to head pointer.
+    void deleteNode(Node *del_node)
+    {
+        //storing the node next to given node in a pointer.	
+    	Node* temp = del_node->next;
+    	
+    	//copying the data of pointer in the given current node.
+    	del_node->data = temp->data;
+    	
+    	//storing the next node to pointer in link part of current node.
+    	del_node->next = temp->next;
+
+    	// freeing memory.
+    	free(temp);
+    	
+    }
+};
