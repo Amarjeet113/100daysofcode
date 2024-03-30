595882fe6ef789daf7f0fa462cc1b1f2e41d4e2c
AMARJEET KUMAR <108997919+Amarjeet113@users.noreply.github.com>
2024-03-30 21:53:43 +0530

    Update q1.cpp

--- 100 days of code/Day68/q1.cpp
+++ 100 days of code/Day68/q1.cpp
@@ -1,1 +1,21 @@
 //   Reverse Linked List  (LEETCODE)
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if(head==NULL || head->next==NULL){
+            return head;
+        }
+        ListNode*prev=NULL;
+        ListNode*curr= head;
+        ListNode*forward =NULL;
+
+        while(curr!=NULL){
+            forward= curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = forward;
+        }
+        return prev;
+    }
+};
