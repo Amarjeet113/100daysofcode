859bccc30c275c91533d5d9843c8095ed850b7ef
AMARJEET KUMAR <108997919+Amarjeet113@users.noreply.github.com>
2024-04-15 23:09:20 +0530

    Update q1.cpp

--- 100 days of code/Day71/q1.cpp
+++ 100 days of code/Day71/q1.cpp
@@ -1,1 +1,66 @@
 //  Reorder List  (LEETCODE)
+
+
+class Solution {
+
+public:
+
+    void reorderList(ListNode* head) {
+
+        stack<ListNode*> s;
+
+        ListNode* curr = head;
+
+        while(curr){
+
+            s.push(curr);
+
+            curr = curr->next;
+
+        }
+
+        curr = head;
+
+        unordered_map<ListNode*, bool> vis;
+
+        while(true){
+
+            ListNode* last = s.top();
+
+            s.pop();
+
+            ListNode* next = curr->next;
+
+            vis[curr] = true;
+
+            if(vis[last]){ 
+
+                curr->next = NULL; 
+
+                break; 
+
+            } 
+
+            curr->next = last; 
+
+            vis[last] = true;
+
+            curr = curr->next; 
+
+            if(vis[next]){
+
+                curr->next = NULL;
+
+                break;
+
+            }
+
+            curr->next = next;
+
+            curr = curr->next;
+
+        }
+
+    }
+
+};
