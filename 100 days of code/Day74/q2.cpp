c277dc268bc9fd03695c11057391f98f025c2378
AMARJEET KUMAR <108997919+Amarjeet113@users.noreply.github.com>
2024-05-02 22:56:40 +0530

    Update q2.cpp

--- 100 days of code/Day74/q2.cpp
+++ 100 days of code/Day74/q2.cpp
@@ -1,1 +1,19 @@
 //  Fibonacci series up to Nth term (GFG).
+
+class Solution {
+  public:
+  int fib(int n){
+      if(n<=1){
+          return n;
+      }
+      return fib(n-1)+fib(n-2);
+  }
+    vector<int> Series(int n) {
+        // Code here
+        vector<int> ans;
+        for(int i=0; i<=n; i++){
+            ans.push_back(fib(i));
+        }
+        return ans;
+    }
+};
