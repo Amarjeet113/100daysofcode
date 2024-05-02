cf46e26e8120ea8c319606102493d73b7a437809
AMARJEET KUMAR <108997919+Amarjeet113@users.noreply.github.com>
2024-05-02 22:58:46 +0530

    Update q1.cpp

--- 100 days of code/Day74/q1.cpp
+++ 100 days of code/Day74/q1.cpp
@@ -1,1 +1,27 @@
-//
+//   Subarray Product Less Than K (Leetcode)
+
+class Solution {
+public:
+    int numSubarrayProductLessThanK(vector<int>& nums, int k) {
+        int n = nums.size();
+        if (n==0) return 0;
+        long long p = 1;
+        int end = 0;
+        int start = 0;
+        int count = 0;
+        while (end<n){
+            p=p*nums[end];
+            while(start<= end && p>=k){
+                p/=nums[start];
+                start++;
+            }
+            if(p<k)
+            {
+                count+= (end-start+1);
+            }
+            end++;
+        }
+        return count;
+        
+    }
+};
