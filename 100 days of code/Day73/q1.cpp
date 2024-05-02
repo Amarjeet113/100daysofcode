3c7bc7957650ffab9c2dac0ab06ce39f0199c360
AMARJEET KUMAR <108997919+Amarjeet113@users.noreply.github.com>
2024-05-02 22:54:06 +0530

    Update q1.cpp

--- 100 days of code/Day73/q1.cpp
+++ 100 days of code/Day73/q1.cpp
@@ -1,1 +1,17 @@
 //  	First Missing Positive(Leetcode)
+
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int x=1;
+         for (int i=0; i<nums.size();i++){
+            if(nums[i]==x){
+                x++;
+            }
+
+        }
+        return x;
+        
+    }
+};
