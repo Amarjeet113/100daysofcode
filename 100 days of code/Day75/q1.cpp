ea594cd0a329f5787b6f11d92e6901e436e67dd5
AMARJEET KUMAR <108997919+Amarjeet113@users.noreply.github.com>
2024-05-03 23:55:38 +0530

    Update q1.cpp

--- 100 days of code/Day75/q1.cpp
+++ 100 days of code/Day75/q1.cpp
@@ -1,1 +1,22 @@
 //  Length of Longest Subarray With at Most K Frequency (LeetCode)
+
+
+class Solution {
+public:
+    int maxSubarrayLength(vector<int>& nums, int k) {
+        int left = 0 , right = 0,ans = 0;
+
+        unordered_map<int,int> map;
+
+        while(left<nums.size() && right<nums.size()){
+            map[nums[right]]++;
+            while(map[nums[right]]>k){
+                map[nums[left]]--;
+                left++;
+            }
+            ans = max(ans,right-left+1);
+            right++;
+        }
+        return ans;
+    }
+};
