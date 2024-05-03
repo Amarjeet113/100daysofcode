8b76b275c298af782f5a602b0e915045b4fc04a9
AMARJEET KUMAR <108997919+Amarjeet113@users.noreply.github.com>
2024-05-03 23:58:41 +0530

    Update q2.cpp

--- 100 days of code/Day75/q2.cpp
+++ 100 days of code/Day75/q2.cpp
@@ -1,1 +1,19 @@
 //   Insert an Element at the Bottom of a Stack  (gfg)
+
+
+class Solution {
+    public Stack<Integer> insertAtBottom(Stack<Integer> St, int X) {
+        recur(St, X);
+        return St;
+    }
+
+    public void recur(Stack<Integer> st, int x) {
+        if (st.isEmpty()) {
+            st.push(x);
+            return;
+        }
+        int p = st.pop();
+        recur(st, x);
+        st.push(p);
+    }
+}
