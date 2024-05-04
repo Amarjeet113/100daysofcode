d8391cc767f26ed35f39c95cedb94f91b8fbb2ea
AMARJEET KUMAR <108997919+Amarjeet113@users.noreply.github.com>
2024-05-04 23:42:50 +0530

    Update q1.cpp

--- 100 days of code/Day76/q1.cpp
+++ 100 days of code/Day76/q1.cpp
@@ -1,1 +1,23 @@
 // 881. Boats To Save People  (leetcode)
+
+
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        // fisrt we have to sort our array
+        sort(people.begin(),people.end());
+        // then iniailise boat = 0;
+        int boats=0;
+        // after that we use two pointer method
+        int left=0, right=people.size()-1;
+        while(left<=right){
+            if(people[left]+people[right]<=limit){
+                left++;
+            }
+            right--;
+            boats++;
+        }
+        return boats;
+
+    }
+};
