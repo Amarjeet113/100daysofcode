de88ca18e472a6a85b0ea4d33c7a93294e7b222e
AMARJEET KUMAR <108997919+Amarjeet113@users.noreply.github.com>
2024-05-06 22:15:22 +0530

    Update Removenodesfromlinkedlist.cpp

--- 100 days of code/Removenodesfromlinkedlist.cpp
+++ 100 days of code/Removenodesfromlinkedlist.cpp
@@ -1,2 +1,25 @@
 // Remove Nodes From Linked List(LEETCODE)
 
+
+class Solution {
+public:
+    ListNode* removeNodes(ListNode* head) {
+        stack<ListNode*>st;
+        ListNode*curr=head;
+        ListNode*prev=NULL;
+        while(curr!=NULL){
+            int val= curr->val;
+            while(!st.empty() && st.top()->val<val)
+            st.pop();
+            st.push(curr);
+            curr=curr->next;
+        }
+        while(!st.empty()){
+            curr=st.top();
+            curr->next=prev;
+            st.pop();
+            prev=curr;
+        }
+        return prev;
+    }
+};
