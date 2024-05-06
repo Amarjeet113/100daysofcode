a289d8f5f344e25d8e78670cbd226f4cf742811d
AMARJEET KUMAR <108997919+Amarjeet113@users.noreply.github.com>
2024-05-06 22:13:41 +0530

    Update printallnodesthatdon'thavesiblings.cpp

--- 100 days of code/printallnodesthatdon'thavesiblings.cpp
+++ 100 days of code/printallnodesthatdon'thavesiblings.cpp
@@ -1,2 +1,27 @@
 //  Print all nodes that don't have sibling  (GFG)
 
+void traversal(Node*node,vector<int> &ans){
+    if(node){
+        if(node->left&& !node->right){
+            ans.push_back(node->left->data);
+        }
+        if(!node->left&& node->right){
+            ans.push_back(node->right->data);
+        }
+        traversal(node->left,ans);
+        traversal(node->right,ans);
+        return ;
+    }
+}
+
+vector<int> noSibling(Node* node)
+{
+    vector<int>ans;
+    traversal(node,ans);
+    if(ans.size()==0){
+        ans.push_back(-1);
+    }
+    sort(ans.begin(),ans.end());
+    return ans;
+}
+
