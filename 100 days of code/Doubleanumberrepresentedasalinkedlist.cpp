539efc4fa3040348a480d49d85b69a7567eedb53
AMARJEET KUMAR <108997919+Amarjeet113@users.noreply.github.com>
2024-05-07 23:27:31 +0530

    Update Doubleanumberrepresentedasalinkedlist.cpp

--- 100 days of code/Doubleanumberrepresentedasalinkedlist.cpp
+++ 100 days of code/Doubleanumberrepresentedasalinkedlist.cpp
@@ -1,4 +1,24 @@
 //  Double a Number represented in a linked list(Leetcode)
 
 
+class Solution {
+public:
+      int multiply(ListNode*head){
+        // base case 
+        if(head==NULL){
+            return 0;
+        }
+        int value = head->val*2+multiply(head->next);
+        head->val= value%10;
+        return value/10;  // carry
+      }
+    ListNode* doubleIt(ListNode* head) {
+        int carry = multiply(head);
+        if(carry)
+        head=new ListNode(carry,head);
+        return head;
+    }
+};
+
+
 
