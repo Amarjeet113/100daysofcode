8614839f51733b4b83d4d829c05b3cf85d680e13
AMARJEET KUMAR <108997919+Amarjeet113@users.noreply.github.com>
2024-05-08 22:50:57 +0530

    Update Roottoleaf.cpp

--- 100 days of code/Roottoleaf.cpp
+++ 100 days of code/Roottoleaf.cpp
@@ -1,29 +1,31 @@
 //  Root To Leaf (GFG)
 
 class Solution {
-public:
-    vector<string> findRelativeRanks(vector<int>& score) {
-        vector<pair<int, int>> scores;
-        for (int i = 0; i < score.size(); i++) {
-            scores.push_back({score[i], i});
-        }
-        sort(scores.begin(), scores.end(), greater<pair<int, int>>());
-        
-        vector<string> result(score.size());
-        for (int i = 0; i < scores.size(); i++) {
-            if (i == 0) {
-                result[scores[i].second] = "Gold Medal";
-            } else if (i == 1) {
-                result[scores[i].second] = "Silver Medal";
-            } else if (i == 2) {
-                result[scores[i].second] = "Bronze Medal";
-            } else {
-                result[scores[i].second] = to_string(i + 1);
-            }
+  public:
+    void solve(Node* root, vector<int>curr, vector<vector<int>>&ans){
+        if(root==NULL){
+            return;
         }
         
-        return result;
+        curr.push_back(root->data);
+        if(root->left==NULL && root->right==NULL){
+            ans.push_back(curr);
+            return ;
+        }
+        
+        solve(root->left, curr, ans);
+        solve(root->right, curr, ans);
+        
+        return;
+    }
+  
+    vector<vector<int>> Paths(Node* root) {
+        // code here
+        vector<vector<int>>ans;
+        vector<int>curr;
+        solve(root, curr, ans);
+        
+        
+        return ans;
     }
 };
-
-
