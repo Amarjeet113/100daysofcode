4f0f89b186c912106939a3b774b179fdd502c23b
AMARJEET KUMAR <108997919+Amarjeet113@users.noreply.github.com>
2024-05-08 22:50:21 +0530

    Update Relativeranks.cpp

--- 100 days of code/Relativeranks.cpp
+++ 100 days of code/Relativeranks.cpp
@@ -1,1 +1,29 @@
 // RELATIVE RANKS (LEETCODE)
+
+class Solution {
+public:
+    vector<string> findRelativeRanks(vector<int>& score) {
+        vector<pair<int, int>> scores;
+        for (int i = 0; i < score.size(); i++) {
+            scores.push_back({score[i], i});
+        }
+        sort(scores.begin(), scores.end(), greater<pair<int, int>>());
+        
+        vector<string> result(score.size());
+        for (int i = 0; i < scores.size(); i++) {
+            if (i == 0) {
+                result[scores[i].second] = "Gold Medal";
+            } else if (i == 1) {
+                result[scores[i].second] = "Silver Medal";
+            } else if (i == 2) {
+                result[scores[i].second] = "Bronze Medal";
+            } else {
+                result[scores[i].second] = to_string(i + 1);
+            }
+        }
+        
+        return result;
+    }
+};
+
+
