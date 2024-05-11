809fff4ed00047c7872aa803ebe2ba37bf120a11
AMARJEET KUMAR <108997919+Amarjeet113@users.noreply.github.com>
2024-05-11 22:21:38 +0530

    Update Juggler Sequence.cpp

--- 100 days of code/Juggler Sequence.cpp
+++ 100 days of code/Juggler Sequence.cpp
@@ -1,1 +1,27 @@
 //  Juggler Sequence  (GFG)
+
+class Solution {
+  public:
+    // Function to generate the Juggler Sequence
+    vector<long long> jugglerSequence(long long n) {
+        // Initializing the result vector with the initial number
+        vector<long long> res;
+        res.emplace_back(n);
+
+        // Loop until the number becomes 1
+        while (n > 1) {
+            // If the number is odd, perform the juggler equation
+            if (n % 2)
+                n = (sqrt(n) * n);
+            // If the number is even, perform the juggler equation
+            else
+                n = sqrt(n);
+
+            // Add the number to the result vector
+            res.emplace_back(n);
+        }
+
+        // Return the final result vector
+        return res;
+    }
+};
